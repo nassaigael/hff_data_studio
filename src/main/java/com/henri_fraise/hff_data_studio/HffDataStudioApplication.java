6c051a5c1032fe6c7e2d81daefa88b77bca16a9b
nassaigael <hei.nassai@gmail.com>
2026-07-10 11:28:09 +0300

    Initialize Spring Boot project with Maven, application entry-point, and basic test structure.

---
+++ src/main/java/com/henri_fraise/hff_data_studio/HffDataStudioApplication.java
@@ -0,0 +1,13 @@
+package com.henri_fraise.hff_data_studio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HffDataStudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HffDataStudioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/henri_fraise/hff_data_studio/HffDataStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henri_fraise.hff_data_studio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HffDataStudioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
