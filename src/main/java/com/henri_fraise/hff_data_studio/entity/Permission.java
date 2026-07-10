07a9452142792b5209d4d2bf0327c6435848a470
nassaigael <hei.nassai@gmail.com>
2026-07-10 14:22:24 +0300

    feat(entity): add `Permission` JPA entity placeholder for future table mapping

---
+++ src/main/java/com/henri_fraise/hff_data_studio/entity/Permission.java
@@ -0,0 +1,15 @@
+package com.henri_fraise.hff_data_studio.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table
+public class Permission {
+
+    //TODO [Reverse Engineering] generate columns from DB
+}
