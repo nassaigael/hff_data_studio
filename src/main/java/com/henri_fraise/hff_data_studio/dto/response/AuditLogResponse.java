6fbaa40b83ec2a1533a0b8f9e68a15c72ec39597
nassaigael <hei.nassai@gmail.com>
2026-07-13 14:09:05 +0300

    feat(dto): add fields to `AuditLogResponse` for enhanced logging

--- src/main/java/com/henri_fraise/hff_data_studio/dto/response/AuditLogResponse.java
+++ src/main/java/com/henri_fraise/hff_data_studio/dto/response/AuditLogResponse.java
@@ -5,10 +5,21 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 public class AuditLogResponse {
-
+	private UUID logId;
+	private String action;
+	private String concernedEntity;
+	private UUID entityId;
+	private LocalDateTime actionDate;
+	private String ipAddress;
+	private UUID userId;
+	private String userFullName;
+	private String userEmail;
 }
