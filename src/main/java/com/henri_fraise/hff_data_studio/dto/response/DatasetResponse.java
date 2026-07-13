afe6ebd78cf0106242da8f18d1cc6785639a263c
nassaigael <hei.nassai@gmail.com>
2026-07-13 13:29:57 +0300

    feat(dto): add lombok annotations to `DatasetResponse`

--- src/main/java/com/henri_fraise/hff_data_studio/dto/response/DatasetResponse.java
+++ src/main/java/com/henri_fraise/hff_data_studio/dto/response/DatasetResponse.java
@@ -1,4 +1,26 @@
 package com.henri_fraise.hff_data_studio.dto.response;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class DatasetResponse {
+    private UUID datasetId;
+    private String datasetName;
+    private Integer rowCount;
+    private Integer columnCount;
+    private LocalDateTime createdAt;
+    private Boolean isCleaned;
+    private UUID fileId;
+    private String fileName;
+    private UUID projectId;
+    private String projectName;
+    private Integer qualityScore;
+    private Integer analysisCount;
 }
