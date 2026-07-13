c0d32b1e72061bff2370c4e62478740de5774baf
nassaigael <hei.nassai@gmail.com>
2026-07-13 13:26:54 +0300

    feat(dto): add lombok annotations to `SourceFileResponse`

--- src/main/java/com/henri_fraise/hff_data_studio/dto/response/SourceFileResponse.java
+++ src/main/java/com/henri_fraise/hff_data_studio/dto/response/SourceFileResponse.java
@@ -1,4 +1,29 @@
 package com.henri_fraise.hff_data_studio.dto.response;
 
+import com.henri_fraise.hff_data_studio.enums.FileProcessingStatus;
+import com.henri_fraise.hff_data_studio.enums.FileType;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class SourceFileResponse {
+    private UUID fileId;
+    private String fileName;
+    private FileType fileType;
+    private String storagePath;
+    private Long sizesBytes;
+    private String sizesFormatted;
+    private LocalDateTime uploadAt;
+    private FileProcessingStatus processingStatus;
+    private UUID projectId;
+    private String projectName;
+    private UUID userId;
+    private String userFullName;
+    private Integer datasetCount;
 }
