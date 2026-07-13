87c1c39d3a73080d74b624b9016b5e5a909e5009
nassaigael <hei.nassai@gmail.com>
2026-07-13 11:10:44 +0300

    feat(enum): add new statuses to `CleaningHistoryStatus` enum

--- src/main/java/com/henri_fraise/hff_data_studio/enums/CleaningHistoryStatus.java
+++ src/main/java/com/henri_fraise/hff_data_studio/enums/CleaningHistoryStatus.java
@@ -1,4 +1,10 @@
 package com.henri_fraise.hff_data_studio.enums;
 
 public enum CleaningHistoryStatus {
+    SUCCESS,
+    FAILURE,
+    PARTIAL_SUCCESS,
+    IN_PROGRESS,
+    CANCELLED,
+    PENDING
 }
