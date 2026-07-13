b81026ef2dacec8386e6453f91844b3f80d66a29
nassaigael <hei.nassai@gmail.com>
2026-07-13 11:24:23 +0300

    feat(dto): add validation annotations to `LoginRequest`

--- src/main/java/com/henri_fraise/hff_data_studio/dto/request/LoginRequest.java
+++ src/main/java/com/henri_fraise/hff_data_studio/dto/request/LoginRequest.java
@@ -1,4 +1,21 @@
 package com.henri_fraise.hff_data_studio.dto.request;
 
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
 }
