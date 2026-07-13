4bf092c5286c829aac67fa5dfb8e53208c0863e7
nassaigael <hei.nassai@gmail.com>
2026-07-13 14:07:14 +0300

    feat(dto): add lombok annotations to `AuditLogResponse`

--- src/main/java/com/henri_fraise/hff_data_studio/dto/response/AuditLogResponse.java
+++ src/main/java/com/henri_fraise/hff_data_studio/dto/response/AuditLogResponse.java
@@ -1,4 +1,14 @@
 package com.henri_fraise.hff_data_studio.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
 public class AuditLogResponse {
+
 }
