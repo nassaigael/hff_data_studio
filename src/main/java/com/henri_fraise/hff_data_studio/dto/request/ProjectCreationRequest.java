b5f742e3f7d42ee323347d0c2735b0aceda432f0
nassaigael <hei.nassai@gmail.com>
2026-07-13 11:40:26 +0300

    feat(dto): add validation and lombok annotations to `ProjectCreationRequest`

--- src/main/java/com/henri_fraise/hff_data_studio/dto/request/ProjectCreationRequest.java
+++ src/main/java/com/henri_fraise/hff_data_studio/dto/request/ProjectCreationRequest.java
@@ -1,4 +1,18 @@
 package com.henri_fraise.hff_data_studio.dto.request;
 
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProjectCreationRequest {
+
+    @NotBlank(message = "Project name is required")
+    private String projectName;
+
+    private String description;
 }
