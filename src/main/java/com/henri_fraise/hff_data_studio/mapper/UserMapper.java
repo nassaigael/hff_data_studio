207b8e9a47807974cfb3ca9911905d34ed0c4a65
nassaigael <hei.nassai@gmail.com>
2026-07-13 14:36:57 +0300

    feat(mapper): add dependencies and annotations to `UserMapper`

--- src/main/java/com/henri_fraise/hff_data_studio/mapper/UserMapper.java
+++ src/main/java/com/henri_fraise/hff_data_studio/mapper/UserMapper.java
@@ -1,4 +1,14 @@
 package com.henri_fraise.hff_data_studio.mapper;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class UserMapper {
+
+	private final UserCategoryMapper userCategoryMapper;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 }
