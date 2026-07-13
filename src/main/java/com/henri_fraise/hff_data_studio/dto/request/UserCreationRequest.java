42b36623ec1a16a9cbbb68da2264fa47af973784
nassaigael <hei.nassai@gmail.com>
2026-07-13 11:31:11 +0300

    feat(dto): add validation and lombok annotations to `UserCreationRequest`

--- src/main/java/com/henri_fraise/hff_data_studio/dto/request/UserCreationRequest.java
+++ src/main/java/com/henri_fraise/hff_data_studio/dto/request/UserCreationRequest.java
@@ -1,4 +1,31 @@
 package com.henri_fraise.hff_data_studio.dto.request;
 
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserCreationRequest {
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @NotBlank(message = "Category is required")
+    private UUID categoryId;
 }
