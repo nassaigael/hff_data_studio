1d2a7cf9b4da258959a54fd0ba0721da5227cf44
nassaigael <hei.nassai@gmail.com>
2026-07-13 14:01:01 +0300

    feat(dto): add lombok annotations to `AnalysisResultResponse`

--- src/main/java/com/henri_fraise/hff_data_studio/dto/response/AnalysisResultResponse.java
+++ src/main/java/com/henri_fraise/hff_data_studio/dto/response/AnalysisResultResponse.java
@@ -1,4 +1,24 @@
 package com.henri_fraise.hff_data_studio.dto.response;
 
+import com.henri_fraise.hff_data_studio.enums.ResultType;
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnalysisResultResponse {
+	private UUID resultId;
+	private ResultType resultType;
+	private String resultTypeLabel;
+	private String title;
+	private String filePath;
+	private String fileFormat;
+	private Integer displayOrder;
+	private UUID executionId;
+	private ChartResponse chart;
+	private String downloadUrl;
 }
