3f4b5ca9f5d328dc9f76f824dccf87513edb6017
nassaigael <hei.nassai@gmail.com>
2026-07-13 11:50:30 +0300

    feat(dto): add validation and lombok annotations to `CleaningRuleRequest`

--- src/main/java/com/henri_fraise/hff_data_studio/dto/request/CleaningRuleRequest.java
+++ src/main/java/com/henri_fraise/hff_data_studio/dto/request/CleaningRuleRequest.java
@@ -1,4 +1,25 @@
 package com.henri_fraise.hff_data_studio.dto.request;
 
+import com.henri_fraise.hff_data_studio.enums.RuleType;
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+import java.util.Map;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CleaningRuleRequest {
+
+    @NotNull(message = "Rule type is required")
+    private RuleType ruleType;
+
+    private Map<String, Object> parametersJson;
+
+    private Integer executionOrder;
+
+    @Builder.Default
+    private Boolean isActive = true;
 }
