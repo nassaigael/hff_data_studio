14add25a7aded5d174aa5c6352407fa0a13cc0c7
nassaigael <hei.nassai@gmail.com>
2026-07-13 16:16:43 +0300

    feat(mapper): annotate `DatasetColumnMapper` with `@Component` and add `DecimalFormat` constant

--- src/main/java/com/henri_fraise/hff_data_studio/mapper/DatasetColumnMapper.java
+++ src/main/java/com/henri_fraise/hff_data_studio/mapper/DatasetColumnMapper.java
@@ -1,4 +1,10 @@
 package com.henri_fraise.hff_data_studio.mapper;
 
+import org.springframework.stereotype.Component;
+
+import java.text.DecimalFormat;
+
+@Component
 public class DatasetColumnMapper {
+	private static final DecimalFormat df = new DecimalFormat("0.00");
 }
