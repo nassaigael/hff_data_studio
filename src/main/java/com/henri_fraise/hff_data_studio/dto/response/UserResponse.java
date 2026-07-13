44e3e67bbe6aea2d973118ae8ed43fe36b19b494
nassaigael <hei.nassai@gmail.com>
2026-07-13 13:15:33 +0300

    feat(dto): add lombok annotations to `UserResponse`

--- src/main/java/com/henri_fraise/hff_data_studio/dto/response/UserResponse.java
+++ src/main/java/com/henri_fraise/hff_data_studio/dto/response/UserResponse.java
@@ -1,4 +1,24 @@
 package com.henri_fraise.hff_data_studio.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
 public class UserResponse {
+    private UUID userId;
+    private String lastName;
+    private String firstName;
+    private String email;
+    private Boolean isActive;
+    private LocalDateTime createdAt;
+    private LocalDateTime lastLogin;
+    private UserCategoryResponse category;
 }
