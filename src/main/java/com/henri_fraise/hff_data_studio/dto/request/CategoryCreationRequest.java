ea17825f123e593a1b3f12d04e6cf22c5e2b9c88
nassaigael <hei.nassai@gmail.com>
2026-07-13 11:39:02 +0300

    feat(dto): add validation and lombok annotations to `CategoryCreationRequest`

--- src/main/java/com/henri_fraise/hff_data_studio/dto/request/CategoryCreationRequest.java
+++ src/main/java/com/henri_fraise/hff_data_studio/dto/request/CategoryCreationRequest.java
@@ -1,4 +1,22 @@
 package com.henri_fraise.hff_data_studio.dto.request;
 
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryCreationRequest {
+
+    @NotBlank(message = "Category label is required")
+    private String label;
+
+    private String description;
+
+    @NotNull(message = "Acces level is required")
+    private Integer accessLevel;
 }
