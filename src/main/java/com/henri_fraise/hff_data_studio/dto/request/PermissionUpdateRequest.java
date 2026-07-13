14437a5b615053edffbdf078de2492b9177d8a73
nassaigael <hei.nassai@gmail.com>
2026-07-13 12:00:12 +0300

    feat(dto): add lombok annotations to `PermissionUpdateRequest`

--- src/main/java/com/henri_fraise/hff_data_studio/dto/request/PermissionUpdateRequest.java
+++ src/main/java/com/henri_fraise/hff_data_studio/dto/request/PermissionUpdateRequest.java
@@ -1,4 +1,16 @@
 package com.henri_fraise.hff_data_studio.dto.request;
 
+import lombok.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PermissionUpdateRequest {
+
+    private List<UUID> permissionIDs;
 }
