8d7415db94a14e225983bf03bad9845a42ed2c71
nassaigael <hei.nassai@gmail.com>
2026-07-14 17:08:00 +0300

    feat(exception): enhance `TokenExpiredException` with additional constructors and error code support

--- src/main/java/com/henri_fraise/hff_data_studio/exception/TokenExpiredException.java
+++ src/main/java/com/henri_fraise/hff_data_studio/exception/TokenExpiredException.java
@@ -1,7 +1,18 @@
 package com.henri_fraise.hff_data_studio.exception;
 
-public class TokenExpiredException extends RuntimeException {
+public class TokenExpiredException extends BusinessException {
+
+	private static final String DEFAULT_ERROR_CODE = "TOKEN_EXPIRED";
+
 	public TokenExpiredException(String message) {
-		super(message);
+		super(message, DEFAULT_ERROR_CODE);
+	}
+
+	public TokenExpiredException() {
+		super("Authentication token has expired. Please login again", DEFAULT_ERROR_CODE);
+	}
+
+	public TokenExpiredException(String message, Throwable cause) {
+		super(message, DEFAULT_ERROR_CODE, cause);
 	}
 }
