7ecde43a31cd5385ac0d67ebe25763ce3ba4ec34
nassaigael <hei.nassai@gmail.com>
2026-07-14 08:45:20 +0300

    feat(mapper): inject dependencies into `AnalysisExecutionMapper`

--- src/main/java/com/henri_fraise/hff_data_studio/mapper/AnalysisExecutionMapper.java
+++ src/main/java/com/henri_fraise/hff_data_studio/mapper/AnalysisExecutionMapper.java
@@ -1,4 +1,12 @@
 package com.henri_fraise.hff_data_studio.mapper;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class AnalysisExecutionMapper {
+
+	private final AnalysisResultMapper resultMapper;
+
 }
