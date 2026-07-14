276f1cd61a34c6cc4faf5cfd3cd28a42b9ccecf1
nassaigael <hei.nassai@gmail.com>
2026-07-14 15:46:30 +0300

    feat(validation): implement `@UniqueProjectName` annotation validator to enforce unique project name constraint

--- src/main/java/com/henri_fraise/hff_data_studio/validation/Constraint/UniqueProjectName.java
+++ src/main/java/com/henri_fraise/hff_data_studio/validation/Constraint/UniqueProjectName.java
@@ -1,4 +1,39 @@
 package com.henri_fraise.hff_data_studio.validation.Constraint;
 
-public interface UniqueProjectName {
+import com.henri_fraise.hff_data_studio.repository.ProjectRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.lang.annotation.*;
+import java.util.UUID;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface UniqueProjectName {
+	String message() default "Project name already exists";
+
+	Class<?>[] groups() default {};
+
+	Class<?>[] payload() default {};
+
+	UUID excludeProjectId() default "0";
+}
+
+@Component
+@RequiredArgsConstructor
+class UniqueProjectNameValidator implements ConstraintValidator<UniqueProjectName, String> {
+
+	private final ProjectRepository projectRepository;
+	private UUID excludeProjectId;
+
+	@Override
+	public boolean isValid(String projectName, ConstraintValidatorContext context) {
+		if (projectName == null || projectName.isEmpty()) return true;
+		if (excludeProjectId != null && excludeProjectId != UUID.fromString("00000000-0000-0000-0000-000000000000"))
+			return !projectRepository.existsByProjectNameAndIdNot(projectName, excludeProjectId);
+		return !projectRepository.existsByProjectName(projectName);
+	}
 }
