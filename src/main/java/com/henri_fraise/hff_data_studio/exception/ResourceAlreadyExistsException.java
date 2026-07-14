7a9c660e65cbe9a621c07f1f75297cc8179b8942
nassaigael <hei.nassai@gmail.com>
2026-07-14 17:05:42 +0300

    feat(exception): enhance `ResourceAlreadyExistsException` with additional constructors and error code support

--- src/main/java/com/henri_fraise/hff_data_studio/exception/ResourceAlreadyExistsException.java
+++ src/main/java/com/henri_fraise/hff_data_studio/exception/ResourceAlreadyExistsException.java
@@ -1,7 +1,22 @@
 package com.henri_fraise.hff_data_studio.exception;
 
-public class ResourceAlreadyExistsException extends RuntimeException {
+public class ResourceAlreadyExistsException extends BusinessException {
+
+	private static final String DEFAULT_ERROR_CODE = "RESOURCE_ALREADY_EXISTS";
+
 	public ResourceAlreadyExistsException(String message) {
-		super(message);
+		super(message, DEFAULT_ERROR_CODE);
+	}
+
+	public ResourceAlreadyExistsException(String entityName, String fieldName, Object fieldValue) {
+		super(
+				String.format("%s already exists with %s: %s", entityName, fieldName, fieldValue),
+				DEFAULT_ERROR_CODE,
+				entityName, fieldName, fieldValue
+		);
+	}
+
+	public ResourceAlreadyExistsException(String message, Throwable cause) {
+		super(message, DEFAULT_ERROR_CODE, cause);
 	}
 }
