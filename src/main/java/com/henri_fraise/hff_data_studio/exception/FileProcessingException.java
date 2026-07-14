7b8e115f31f36b7ab7223d09c989b4b0345cd826
nassaigael <hei.nassai@gmail.com>
2026-07-14 17:09:21 +0300

    feat(exception): enhance `FileProcessingException` with additional constructors and error code support

--- src/main/java/com/henri_fraise/hff_data_studio/exception/FileProcessingException.java
+++ src/main/java/com/henri_fraise/hff_data_studio/exception/FileProcessingException.java
@@ -1,7 +1,22 @@
 package com.henri_fraise.hff_data_studio.exception;
 
-public class FileProcessingException extends RuntimeException {
+public class FileProcessingException extends BusinessException {
+
+	private static final String DEFAULT_ERROR_CODE = "FILE_PROCESSING_ERROR";
+
 	public FileProcessingException(String message) {
-		super(message);
+		super(message, DEFAULT_ERROR_CODE);
+	}
+
+	public FileProcessingException(String fileName, String reason) {
+		super(
+				String.format("Error processing file '%s': %s", fileName, reason),
+				DEFAULT_ERROR_CODE,
+				fileName, reason
+		);
+	}
+
+	public FileProcessingException(String message, Throwable cause) {
+		super(message, DEFAULT_ERROR_CODE, cause);
 	}
 }
