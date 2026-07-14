05e4726e7f4c9c81ed61d63e0834483b69c8ddcc
nassaigael <hei.nassai@gmail.com>
2026-07-14 17:07:11 +0300

    feat(exception): enhance `InvalidCredentialsException` with additional constructors and error code support

--- src/main/java/com/henri_fraise/hff_data_studio/exception/InvalidCredentialsException.java
+++ src/main/java/com/henri_fraise/hff_data_studio/exception/InvalidCredentialsException.java
@@ -1,7 +1,18 @@
 package com.henri_fraise.hff_data_studio.exception;
 
-public class InvalidCredentialsException extends RuntimeException {
+public class InvalidCredentialsException extends BusinessException {
+
+	private static final String DEFAULT_ERROR_CODE = "INVALID_CREDENTIALS";
+
 	public InvalidCredentialsException(String message) {
-		super(message);
+		super(message, DEFAULT_ERROR_CODE);
+	}
+
+	public InvalidCredentialsException() {
+		super("Invalid email or password", DEFAULT_ERROR_CODE);
+	}
+
+	public InvalidCredentialsException(String message, Throwable cause) {
+		super(message, DEFAULT_ERROR_CODE, cause);
 	}
 }
