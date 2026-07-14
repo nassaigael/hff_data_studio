32e47d46733d949983ad1262ef1836d6f2a94c0d
nassaigael <hei.nassai@gmail.com>
2026-07-14 11:23:48 +0300

    feat(validation): implement `@ValidEmail` annotation for email validation with customizable options

--- src/main/java/com/henri_fraise/hff_data_studio/validation/Annotation/ValidEmail.java
+++ src/main/java/com/henri_fraise/hff_data_studio/validation/Annotation/ValidEmail.java
@@ -1,4 +1,34 @@
 package com.henri_fraise.hff_data_studio.validation.Annotation;
 
-public interface ValidEmail {
+import com.networknt.org.apache.commons.validator.routines.EmailValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Constraint(validatedBy = EmailValidator.class)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidEmail {
+
+	String DEFAULT_MESSAGE = "Email must be valid and from an allowed domain";
+
+	String message() default DEFAULT_MESSAGE;
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+
+	boolean nullable() default false;
+
+	boolean blankAllowed() default false;
+
+	boolean emptyAllowed() default false;
+
+	String[] domains() default {};
 }
