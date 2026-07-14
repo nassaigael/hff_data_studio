67271f3e5b9c0845058fc3f88c31fa8bf86230fc
nassaigael <hei.nassai@gmail.com>
2026-07-14 15:36:07 +0300

    feat(validation): implement `@UniqueEmail` annotation validator to enforce unique email constraint

--- src/main/java/com/henri_fraise/hff_data_studio/validation/Constraint/UniqueEmail.java
+++ src/main/java/com/henri_fraise/hff_data_studio/validation/Constraint/UniqueEmail.java
@@ -1,4 +1,34 @@
 package com.henri_fraise.hff_data_studio.validation.Constraint;
 
-public interface UniqueEmail {
+import com.henri_fraise.hff_data_studio.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface UniqueEmail {
+
+	String message() default "Email already exists";
+
+	Class<?>[] groups() default {};
+
+	Class<?>[] payload() default {};
+}
+
+@Component
+@RequiredArgsConstructor
+class UniqueEmailValidator implements ConstraintValidator<UniqueEmail, String> {
+
+	private final UserRepository userRepository;
+
+	@Override
+	public boolean isValid(String email, ConstraintValidatorContext context) {
+		if (email == null || email.isEmpty()) return true;
+		return !userRepository.existsByEmail(email);
+	}
 }
