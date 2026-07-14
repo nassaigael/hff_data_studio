5ad35fefd335bd9d5754f8f6a9b7a435cbb7c80b
nassaigael <hei.nassai@gmail.com>
2026-07-14 09:55:29 +0300

    feat(mapper): implement `toResponse` method in `ExportMapper` for entity-to-DTO conversion

--- src/main/java/com/henri_fraise/hff_data_studio/mapper/ExportMapper.java
+++ src/main/java/com/henri_fraise/hff_data_studio/mapper/ExportMapper.java
@@ -1,7 +1,39 @@
 package com.henri_fraise.hff_data_studio.mapper;
 
+import com.henri_fraise.hff_data_studio.dto.response.ExportResponse;
+import com.henri_fraise.hff_data_studio.entity.Export;
+import com.henri_fraise.hff_data_studio.enums.AnalysisExecutionStatus;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ExportMapper {
+
+	public ExportResponse toResponse(Export export) {
+		String EXPORT_PATH = "/api/v1/exports/";
+		if (export == null) return null;
+
+		return ExportResponse.builder()
+				.exportId(export.getId())
+				.exportFormat(export.getExportFomat())
+				.formatLabel(getFormatLabel(export.getExportFomat()))
+				.exportedAt(export.getExportAt())
+				.filePath(export.getFilePath())
+				.fileName(extractFileName(export.getFilePath()))
+				.fileSize(export.getFileSize() != null ? export.getFileSize() : null)
+				.fileSizeFormatted(formatFileSize(export.getFileSize()))
+				.executionId(export.getExecution() != null ? export.getExecution().getId() : null)
+				.executionStatus(
+						export.getExecution() != null && export.getExecution().getStatus() != null
+								? AnalysisExecutionStatus.valueOf(export.getExecution().getStatus().name())
+								: null
+				)
+				.userId(export.getUser() != null ? export.getUser().getId() : null)
+				.userFullName(
+						export.getUser() != null
+								? export.getUser().getFirstName() + " " + export.getUser().getLastName()
+								: null
+				)
+				.downloadUrl(EXPORT_PATH + export.getId() + "/download")
+				.build();
+	}
 }
