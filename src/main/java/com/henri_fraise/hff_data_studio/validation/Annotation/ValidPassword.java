bf9fb319daf82b7279df5792dcd22ee557d52ee9
nassaigael <hei.nassai@gmail.com>
2026-07-14 11:31:45 +0300

    feat(validation): implement `@ValidPassword` annotation for password validation with customizable constraints

--- src/main/java/com/henri_fraise/hff_data_studio/validation/Annotation/ValidPassword.java
+++ src/main/java/com/henri_fraise/hff_data_studio/validation/Annotation/ValidPassword.java
@@ -1,4 +1,32 @@
 package com.henri_fraise.hff_data_studio.validation.Annotation;
 
-public interface ValidPassword {
+import com.henri_fraise.hff_data_studio.validation.Validator.PasswordValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = PasswordValidator.class)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidPassword {
+
+	String message() default "Password must be at least 8 characters, contain uppercase, lowercase, digit and character";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+
+	int minLength() default 8;
+
+	int maxLength() default 32;
+
+	boolean requireUppercase() default true;
+
+	boolean requireLowercase() default true;
+
+	boolean requireDigit() default true;
+
+	boolean requireSpecialChar() default true;
 }
