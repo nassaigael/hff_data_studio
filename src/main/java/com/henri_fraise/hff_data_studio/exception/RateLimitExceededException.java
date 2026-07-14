859d10efa1aa5a2cb3a23abd017e7b47cc0d4866
nassaigael <hei.nassai@gmail.com>
2026-07-14 17:11:28 +0300

    feat(exception): enhance `RateLimitExceededException` with additional constructors and error code support

--- src/main/java/com/henri_fraise/hff_data_studio/exception/RateLimitExceededException.java
+++ src/main/java/com/henri_fraise/hff_data_studio/exception/RateLimitExceededException.java
@@ -1,7 +1,18 @@
 package com.henri_fraise.hff_data_studio.exception;
 
-public class RateLimitExceededException extends RuntimeException {
+public class RateLimitExceededException extends BusinessException {
+
+	private static final String DEFAULT_ERROR_CODE = "RATE_LIMIT_EXCEEDED";
+
 	public RateLimitExceededException(String message) {
-		super(message);
+		super(message, DEFAULT_ERROR_CODE);
+	}
+
+	public RateLimitExceededException() {
+		super("Too many requests. Please try again later", DEFAULT_ERROR_CODE);
+	}
+
+	public RateLimitExceededException(String message, Throwable cause) {
+		super(message, DEFAULT_ERROR_CODE, cause);
 	}
 }
