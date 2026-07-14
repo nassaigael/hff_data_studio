2cd70fa423dfdb544eeee9a262e21ce297da1f27
nassaigael <hei.nassai@gmail.com>
2026-07-14 17:10:38 +0300

    feat(exception): enhance `CleaningException` with additional constructors and error code support

--- src/main/java/com/henri_fraise/hff_data_studio/exception/CleaningException.java
+++ src/main/java/com/henri_fraise/hff_data_studio/exception/CleaningException.java
@@ -1,7 +1,22 @@
 package com.henri_fraise.hff_data_studio.exception;
 
-public class CleaningException extends RuntimeException {
+public class CleaningException extends BusinessException {
+
+	private static final String DEFAULT_ERROR_CODE = "CLEANING_ERROR";
+
 	public CleaningException(String message) {
-		super(message);
+		super(message, DEFAULT_ERROR_CODE);
+	}
+
+	public CleaningException(String datasetName, String reason) {
+		super(
+				String.format("Error cleaning dataset '%s': %s", datasetName, reason),
+				DEFAULT_ERROR_CODE,
+				datasetName, reason
+		);
+	}
+
+	public CleaningException(String message, Throwable cause) {
+		super(message, DEFAULT_ERROR_CODE, cause);
 	}
 }
