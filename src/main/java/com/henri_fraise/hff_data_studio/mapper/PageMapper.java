ece743c84601253c48e4387b45dd8360bfe11d5d
nassaigael <hei.nassai@gmail.com>
2026-07-14 10:28:13 +0300

    feat(mapper): add overloaded `toPageResponse` method in `PageMapper` for flexible entity-to-DTO mapping

--- src/main/java/com/henri_fraise/hff_data_studio/mapper/PageMapper.java
+++ src/main/java/com/henri_fraise/hff_data_studio/mapper/PageMapper.java
@@ -38,4 +38,37 @@ public class PageMapper {
 				.hasPrevious(page.hasPrevious())
 				.build();
 	}
+
+	public <T, R> PageResponse<R> toPageResponse(
+			List<T> content,
+			Page<T> page,
+			Function<T, R> mapper
+	) {
+		if (page == null || content == null)
+			return PageResponse.<R>builder()
+					.content(List.of())
+					.pageNumber(0)
+					.pageSize(20)
+					.totalElements(0L)
+					.totalPages(0)
+					.isFirst(true)
+					.isLast(true)
+					.hasNext(false)
+					.hasPrevious(false)
+					.build();
+
+		List<R> mappedContent = content.stream().map(mapper).toList();
+
+		return PageResponse.<R>builder()
+				.content(mappedContent)
+				.pageNumber(page.getNumber())
+				.pageSize(page.getSize())
+				.totalElements(page.getTotalElements())
+				.totalPages(page.getTotalPages())
+				.isFirst(page.isFirst())
+				.isLast(page.isLast())
+				.hasNext(page.hasNext())
+				.hasPrevious(page.hasPrevious())
+				.build();
+	}
 }
