7635d2fba1f02d04bad89bec02b04083072c263b
nassaigael <hei.nassai@gmail.com>
2026-07-14 09:09:16 +0300

    refactor(mapper): add `chartMapper` dependency to `AnalysisResultMapper`

--- src/main/java/com/henri_fraise/hff_data_studio/mapper/AnalysisResultMapper.java
+++ src/main/java/com/henri_fraise/hff_data_studio/mapper/AnalysisResultMapper.java
@@ -1,7 +1,11 @@
 package com.henri_fraise.hff_data_studio.mapper;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class AnalysisResultMapper {
+
+	private final ChartMapper chartMapper;
 }
