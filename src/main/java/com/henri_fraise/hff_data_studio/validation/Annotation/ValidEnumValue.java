e310d3a57f64815babb7f880d1a2766d26dd9e53
nassaigael <hei.nassai@gmail.com>
2026-07-14 11:40:52 +0300

    feat(validation): implement `@ValidEnumValue` annotation for enum validation with customizable constraints

--- src/main/java/com/henri_fraise/hff_data_studio/validation/Annotation/ValidEnumValue.java
+++ src/main/java/com/henri_fraise/hff_data_studio/validation/Annotation/ValidEnumValue.java
@@ -1,4 +1,25 @@
 package com.henri_fraise.hff_data_studio.validation.Annotation;
 
-public interface ValidEnumValue {
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = EnumValueValidator.class)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidEnumValue {
+
+	String message() default "Invalid enum value";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+
+	Class<? extends Enum<?>> enumClass();
+
+	boolean ignoreCase() default false;
+
+	boolean allowNull() default false;
 }
