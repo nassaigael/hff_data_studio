97784e722c5bbb7576120fba3cb60271a56859f9
nassaigael <hei.nassai@gmail.com>
2026-07-14 09:19:04 +0300

    feat(mapper): implement `toResponse` method in `AnalysisResultMapper` for entity-to-DTO conversion

--- src/main/java/com/henri_fraise/hff_data_studio/mapper/AnalysisResultMapper.java
+++ src/main/java/com/henri_fraise/hff_data_studio/mapper/AnalysisResultMapper.java
@@ -1,5 +1,8 @@
 package com.henri_fraise.hff_data_studio.mapper;
 
+import com.henri_fraise.hff_data_studio.dto.response.AnalysisResultResponse;
+import com.henri_fraise.hff_data_studio.dto.response.ChartResponse;
+import com.henri_fraise.hff_data_studio.entity.AnalysisResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -8,4 +11,27 @@ import org.springframework.stereotype.Component;
 public class AnalysisResultMapper {
 
 	private final ChartMapper chartMapper;
+
+	public AnalysisResultResponse toResponse(AnalysisResult result) {
+
+		if (result == null) return null;
+
+		ChartResponse chartResponse = null;
+		if(result.getChart() != null)
+			chartResponse = chartMapper.toResponse(result.getChart());
+
+		String DOWNLOAD_URL = "/api/v1/results/";
+		return AnalysisResultResponse.builder()
+				.resultId(result.getId())
+				.resultType(result.getResultType())
+				.resultTypeLabel(getResultTypeLabel(result.getResultType()))
+				.title(result.getTitle())
+				.filePath(result.getFilePath())
+				.fileFormat(result.getFileFormat())
+				.displayOrder(result.getDisplayOrder())
+				.executionId(result.getExecution() != null ? result.getExecution().getId() : null)
+				.chart(chartResponse)
+				.downloadUrl(DOWNLOAD_URL + result.getId() + "download")
+				.build();
+	}
 }
