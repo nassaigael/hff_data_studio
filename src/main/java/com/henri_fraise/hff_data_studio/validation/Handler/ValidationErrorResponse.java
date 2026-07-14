2d6087618eea2c44de000d2d850c6c6e0fb9df87
nassaigael <hei.nassai@gmail.com>
2026-07-14 16:15:51 +0300

    feat(validation): add `ValidationErrorResponse` class with builder pattern and nested `FieldError` class

--- src/main/java/com/henri_fraise/hff_data_studio/validation/Handler/ValidationErrorResponse.java
+++ src/main/java/com/henri_fraise/hff_data_studio/validation/Handler/ValidationErrorResponse.java
@@ -1,4 +1,40 @@
 package com.henri_fraise.hff_data_studio.validation.Handler;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ValidationErrorResponse {
+
+	@Builder.Default
+	private LocalDateTime timestamp = LocalDateTime.now();
+
+	private Integer status;
+
+	private String error;
+
+	private String message;
+
+	private String path;
+
+	private List<FieldError> errors;
+
+	private Map<String, String> validationErrors;
+
+	@Data
+	@Builder
+	@AllArgsConstructor
+	@NoArgsConstructor
+	public static class FieldError {
+		private String field;
+		private String rejectedValue;
+		private String message;
+	}
 }
