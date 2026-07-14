a6080ca572ef04f0cfb3894f8c39b9da919a47bf
nassaigael <hei.nassai@gmail.com>
2026-07-14 17:04:34 +0300

    feat(exception): enhance `BusinessException` with error codes and arguments

--- src/main/java/com/henri_fraise/hff_data_studio/exception/BusinessException.java
+++ src/main/java/com/henri_fraise/hff_data_studio/exception/BusinessException.java
@@ -1,7 +1,40 @@
 package com.henri_fraise.hff_data_studio.exception;
 
-public class BusinessException extends RuntimeException {
-	public BusinessException(String message) {
+import lombok.Getter;
+
+@Getter
+public abstract class BusinessException extends RuntimeException {
+
+	private final String errorCode;
+	private final Object[] args;
+
+	protected BusinessException(String message) {
 		super(message);
+		this.errorCode = "BUSINESS_ERROR";
+		this.args = null;
+	}
+
+	protected BusinessException(String message, String errorCode) {
+		super(message);
+		this.errorCode = errorCode;
+		this.args = null;
+	}
+
+	protected BusinessException(String message, Throwable cause) {
+		super(message, cause);
+		this.errorCode = "BUSINESS_ERROR";
+		this.args = null;
+	}
+
+	protected BusinessException(String message, String errorCode, Object... args) {
+		super(message);
+		this.errorCode = errorCode;
+		this.args = args;
+	}
+
+	protected BusinessException(String message, String errorCode, Throwable cause) {
+		super(message, cause);
+		this.errorCode = errorCode;
+		this.args = null;
 	}
 }
