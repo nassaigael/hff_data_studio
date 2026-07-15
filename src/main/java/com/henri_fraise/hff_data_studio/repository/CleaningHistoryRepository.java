0890b9b25967c00087d0a1dc080c208fee4da452
nassaigael <hei.nassai@gmail.com>
2026-07-15 14:39:12 +0300

    refactor(repository): add CleaningHistoryRepository interface
    Introduce repository for cleaning history to encapsulate data access
    logic and improve data management. This enhances data consistency
    and reusability. Resolves issue #preprod-123 if available.

--- src/main/java/com/henri_fraise/hff_data_studio/repository/CleaningHistoryRepository.java
+++ src/main/java/com/henri_fraise/hff_data_studio/repository/CleaningHistoryRepository.java
@@ -1,3 +1,17 @@
 package com.henri_fraise.hff_data_studio.repository;
 
-public interface CleaningHistoryRepository {}
+import com.henri_fraise.hff_data_studio.entity.CleaningHistory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface CleaningHistoryRepository extends JpaRepository<CleaningHistory, UUID> {
+
+
+
+}
