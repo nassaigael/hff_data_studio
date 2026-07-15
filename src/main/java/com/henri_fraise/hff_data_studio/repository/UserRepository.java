fa78b21246c1fa1512a1ef71046f1109f446c914
nassaigael <hei.nassai@gmail.com>
2026-07-15 09:06:04 +0300

    chore(repository): extend `UserRepository` with additional query methods for flexible user retrieval

--- src/main/java/com/henri_fraise/hff_data_studio/repository/UserRepository.java
+++ src/main/java/com/henri_fraise/hff_data_studio/repository/UserRepository.java
@@ -1,11 +1,29 @@
 package com.henri_fraise.hff_data_studio.repository;
 
 import com.henri_fraise.hff_data_studio.entity.User;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-  boolean existsByEmail(String email);
+
+	Optional<User> findByEmail(String email);
+	Optional<User> findByEmailIsAndIsActiveTrue(String email);
+	Page<User> findAllByIsActiveTrue(Pageable pageable);
+	List<User> findAllByIsActiveTrue();
+	Page<User> findByIsActiveTrueAndLastNameContainingIgnoreCaseOrFirstNameContainingIgnoreCase(String lastName, String firstName, Pageable pageable);
+	Page<User> findByCategory_Id(UUID categoryId, Pageable pageable);
+	List<User> findByLastLoginBefore(LocalDateTime date);
+
+	boolean existsByEmail(String email);
+
+
 }
