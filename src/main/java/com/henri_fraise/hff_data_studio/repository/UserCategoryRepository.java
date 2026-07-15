d19d3dbd835747f7172741ac080159a435d859b2
nassaigael <hei.nassai@gmail.com>
2026-07-15 09:47:28 +0300

    chore(repository): add method to update access level in `UserCategoryRepository`

--- src/main/java/com/henri_fraise/hff_data_studio/repository/UserCategoryRepository.java
+++ src/main/java/com/henri_fraise/hff_data_studio/repository/UserCategoryRepository.java
@@ -3,8 +3,10 @@ package com.henri_fraise.hff_data_studio.repository;
 import com.henri_fraise.hff_data_studio.entity.User;
 import com.henri_fraise.hff_data_studio.entity.UserCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,5 +26,8 @@ public interface UserCategoryRepository extends JpaRepository<UserCategory, UUID
 	@Query("SELECT c FROM UserCategory  c WHERE  c.accessLevel <= :maxAccessLevel")
 	List<UserCategory> findByMaxAccessLevel(@Param("maxAccessLevel") Integer maxAccessLevel);
 
-
+	@Modifying
+	@Transactional
+	@Query("UPDATE UserCategory  c SET c.accessLevel = :accessLevel WHERE c.id = :category_id")
+	void updateAccessLevel(@Param("category_id") UUID categoryId, @Param("accessLevel") Integer accessLevel);
 }
