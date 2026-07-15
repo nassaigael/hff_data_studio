d49bde42a59b439660b8ba808ddad74c81d11553
nassaigael <hei.nassai@gmail.com>
2026-07-15 09:45:32 +0300

    chore(repository): add query methods for retrieving categories with most active users and by max access level in `UserCategoryRepository`

--- src/main/java/com/henri_fraise/hff_data_studio/repository/UserCategoryRepository.java
+++ src/main/java/com/henri_fraise/hff_data_studio/repository/UserCategoryRepository.java
@@ -1,7 +1,10 @@
 package com.henri_fraise.hff_data_studio.repository;
 
+import com.henri_fraise.hff_data_studio.entity.User;
 import com.henri_fraise.hff_data_studio.entity.UserCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,4 +17,12 @@ public interface UserCategoryRepository extends JpaRepository<UserCategory, UUID
 
 	boolean existsByLabel(String label);
 	boolean existsByLabelAndIdNot(String label, UUID id);
+
+	@Query("SELECT c FROM UserCategory c JOIN c.users u WHERE u.isActive = true GROUP BY c ORDER BY COUNT(u) DESC")
+	List<UserCategory> findCategoriesWithMostActiveUsers();
+
+	@Query("SELECT c FROM UserCategory  c WHERE  c.accessLevel <= :maxAccessLevel")
+	List<UserCategory> findByMaxAccessLevel(@Param("maxAccessLevel") Integer maxAccessLevel);
+
+
 }
