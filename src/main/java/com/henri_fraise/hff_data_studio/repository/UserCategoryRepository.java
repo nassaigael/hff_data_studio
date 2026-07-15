283930ce99c735ef5717907fab3bd183f8e3af5a
nassaigael <hei.nassai@gmail.com>
2026-07-15 09:48:59 +0300

    chore(repository): format `UserCategoryRepository` for improved readability and consistent styling

--- src/main/java/com/henri_fraise/hff_data_studio/repository/UserCategoryRepository.java
+++ src/main/java/com/henri_fraise/hff_data_studio/repository/UserCategoryRepository.java
@@ -1,33 +1,37 @@
 package com.henri_fraise.hff_data_studio.repository;
 
-import com.henri_fraise.hff_data_studio.entity.User;
 import com.henri_fraise.hff_data_studio.entity.UserCategory;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-
 public interface UserCategoryRepository extends JpaRepository<UserCategory, UUID> {
-	Optional<UserCategory> findByLabel(String label);
-	List<UserCategory> findAllByOrderByAccessLevelDesc();
-	List<UserCategory> findByAccessLevelGreaterThanEqual(Integer accessLevel);
+  Optional<UserCategory> findByLabel(String label);
 
-	boolean existsByLabel(String label);
-	boolean existsByLabelAndIdNot(String label, UUID id);
+  List<UserCategory> findAllByOrderByAccessLevelDesc();
 
-	@Query("SELECT c FROM UserCategory c JOIN c.users u WHERE u.isActive = true GROUP BY c ORDER BY COUNT(u) DESC")
-	List<UserCategory> findCategoriesWithMostActiveUsers();
+  List<UserCategory> findByAccessLevelGreaterThanEqual(Integer accessLevel);
 
-	@Query("SELECT c FROM UserCategory  c WHERE  c.accessLevel <= :maxAccessLevel")
-	List<UserCategory> findByMaxAccessLevel(@Param("maxAccessLevel") Integer maxAccessLevel);
+  boolean existsByLabel(String label);
 
-	@Modifying
-	@Transactional
-	@Query("UPDATE UserCategory  c SET c.accessLevel = :accessLevel WHERE c.id = :category_id")
-	void updateAccessLevel(@Param("category_id") UUID categoryId, @Param("accessLevel") Integer accessLevel);
+  boolean existsByLabelAndIdNot(String label, UUID id);
+
+  @Query(
+      "SELECT c FROM UserCategory c JOIN c.users u WHERE u.isActive = true GROUP BY c ORDER BY"
+          + " COUNT(u) DESC")
+  List<UserCategory> findCategoriesWithMostActiveUsers();
+
+  @Query("SELECT c FROM UserCategory  c WHERE  c.accessLevel <= :maxAccessLevel")
+  List<UserCategory> findByMaxAccessLevel(@Param("maxAccessLevel") Integer maxAccessLevel);
+
+  @Modifying
+  @Transactional
+  @Query("UPDATE UserCategory  c SET c.accessLevel = :accessLevel WHERE c.id = :category_id")
+  void updateAccessLevel(
+      @Param("category_id") UUID categoryId, @Param("accessLevel") Integer accessLevel);
 }
