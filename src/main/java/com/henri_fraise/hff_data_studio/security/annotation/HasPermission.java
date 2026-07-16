2a5a647e30e4ae70907b11f70f0ffa88287c0942
nassaigael <hei.nassai@gmail.com>
2026-07-16 16:27:50 +0300

    feat(security): introduce HasPermission annotation
    Add annotation to restrict access to methods
    based on user permissions and operations.
    Improve security and authorization.
    Resolves issue #123 from preprod branch.

--- src/main/java/com/henri_fraise/hff_data_studio/security/annotation/HasPermission.java
+++ src/main/java/com/henri_fraise/hff_data_studio/security/annotation/HasPermission.java
@@ -1,4 +1,15 @@
 package com.henri_fraise.hff_data_studio.security.annotation;
 
-public class HasPermission {
+import java.lang.annotation.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface HasPermission {
+
+	String value();
+
+	String entity() default "";
+
+	String operation() default "READ";
 }
