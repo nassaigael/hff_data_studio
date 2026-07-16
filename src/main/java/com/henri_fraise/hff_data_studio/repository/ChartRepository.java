56e0844eda3be18691f3b63a12688e0687d5342a
nassaigael <hei.nassai@gmail.com>
2026-07-16 14:06:23 +0300

    feat(repository): add chart queries
    Added new queries to the ChartRepository to retrieve charts by execution ID,
    chart type, and execution ID combined. Also, added a query to count charts
    by execution ID and chart type, and another to fetch all distinct chart types.
    These changes enhance the data retrieval capabilities of the repository.

--- src/main/java/com/henri_fraise/hff_data_studio/repository/ChartRepository.java
+++ src/main/java/com/henri_fraise/hff_data_studio/repository/ChartRepository.java
@@ -3,6 +3,8 @@ package com.henri_fraise.hff_data_studio.repository;
 import com.henri_fraise.hff_data_studio.entity.Chart;
 import com.henri_fraise.hff_data_studio.enums.ChartType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,5 +19,17 @@ public interface ChartRepository  extends JpaRepository<Chart, UUID> {
 	List<Chart> findByChartType(ChartType chartType);
 
 	long countByChartType(ChartType chartType);
-	
+
+	@Query("SELECT c FROM Chart c WHERE c.result.execution.id = :execution_id")
+	List<Chart> findByExecutionId(@Param("execution_id") UUID executionId);
+
+	@Query("SELECT  c FROM Chart c WHERE c.result.execution.id = :execution_id AND c.chartType = :chart_type")
+	List<Chart> findByExecutionIdAndChartType(@Param("execution_id") UUID executionId, @Param("chart_type") ChartType chartType);
+
+	@Query("SELECT  COUNT(c) FROM Chart c WHERE c.chartType = :chart_type AND c.result.execution.id = :execution_id")
+	long countByExecutionIdAndChartType(@Param("execution_id") UUID executionId, @Param("chart_type") ChartType chartType);
+
+	@Query("SELECT DISTINCT c.chartType FROM Chart c")
+	List<Chart> findAllDistinctChartTypes();
+
 }
