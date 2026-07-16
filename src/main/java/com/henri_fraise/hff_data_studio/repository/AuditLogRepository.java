ef334123812a8292d78bef7c19e0ad44e506f1fd
nassaigael <hei.nassai@gmail.com>
2026-07-16 14:33:27 +0300

    feat(repository): add AuditLogRepository interface
    -
    -
    -
    The changes include the introduction of the AuditLogRepository interface, extending JpaRepository for AuditLog entities with UUID as the primary key, and adding the necessary imports and annotations.

--- src/main/java/com/henri_fraise/hff_data_studio/repository/AuditLogRepository.java
+++ src/main/java/com/henri_fraise/hff_data_studio/repository/AuditLogRepository.java
@@ -1,3 +1,15 @@
 package com.henri_fraise.hff_data_studio.repository;
 
-public interface AuditLogRepository {}
+
+import com.henri_fraise.hff_data_studio.entity.AuditLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AuditLogRepository extends JpaRepository<AuditLog, UUID> {
+
+
+
+}
