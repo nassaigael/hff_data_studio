ae1d206e4369ae010258960e84588e22675feaf0
nassaigael <hei.nassai@gmail.com>
2026-07-16 13:51:20 +0300

    feat(repository): add custom queries for AnalysisResult
    Add queries to filter AnalysisResult by execution id and
    result type to improve data retrieval efficiency. This
    enhances the repository's functionality and supports
    various result types. Resolves issue #<issue_number> from
    preprod branch.

--- src/main/java/com/henri_fraise/hff_data_studio/repository/AnalysisResultRepository.java
+++ src/main/java/com/henri_fraise/hff_data_studio/repository/AnalysisResultRepository.java
@@ -3,6 +3,8 @@ package com.henri_fraise.hff_data_studio.repository;
 import com.henri_fraise.hff_data_studio.entity.AnalysisResult;
 import com.henri_fraise.hff_data_studio.enums.ResultType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -20,5 +22,17 @@ public interface AnalysisResultRepository extends JpaRepository<AnalysisResult, 
 	long countByExecutionId(UUID executionId);
 
 	long countByExecutionIdAndResultType(UUID executionId, ResultType resultType);
+
+	@Query("SELECT r FROM AnalysisResult r WHERE r.execution.id = :execution_id AND r.resultType = 'CHART'")
+	List<AnalysisResult> findChartByExecutionId(@Param("execution_id") UUID executionId);
+
+	@Query("SELECT r FROM AnalysisResult r WHERE r.execution.id = :execution_id AND r.resultType = 'TABLE'")
+	List<AnalysisResult> findTableByExecutionId(@Param("execution_id") UUID executionId);
+
+	@Query("SELECT r FROM AnalysisResult  r WHERE r.execution.id = :execution_id AND r.resultType = 'KPI'")
+	List<AnalysisResult> findKPIByExecutionId(@Param("execution_id") UUID executionId);
+
+	@Query("SELECT r FROM AnalysisResult r WHERE r.fileFormat = :file_format AND r.execution.id = :execution_id")
+	List<AnalysisResult> findByExecutionIdAndFileFormat(@Param("execution_id") UUID executionId, @Param("file_format") String fileFormat);
 	
 }
