35e8249dc3a4f9ac5d0c7e70641c7e9e361f6bd9
nassaigael <hei.nassai@gmail.com>
2026-07-16 15:35:59 +0300

    feat(security): enable web security configuration
    Add authentication and authorization to improve application
    security. Include access denied and authentication entry point
    handlers. Resolves issue from preprod branch.

--- src/main/java/com/henri_fraise/hff_data_studio/security/config/SecurityConfig.java
+++ src/main/java/com/henri_fraise/hff_data_studio/security/config/SecurityConfig.java
@@ -1,4 +1,23 @@
 package com.henri_fraise.hff_data_studio.security.config;
 
+import com.henri_fraise.hff_data_studio.security.filter.JwtAuthenticationFilter;
+import com.henri_fraise.hff_data_studio.security.handler.CustomAccessDeniedHandler;
+import com.henri_fraise.hff_data_studio.security.handler.CustomAuthenticationEntryPoint;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+	private final JwtAuthenticationFilter jwtAuthFilter;
+	private final UserDetailsService userDetailsService;
+	private final CustomAuthenticationEntryPoint authenticationEntryPoint;
+	private final CustomAccessDeniedHandler accessDeniedHandler;
+
 }
