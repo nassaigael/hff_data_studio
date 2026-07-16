02be9987dcde33b7da589d5cc553df03c623f2e6
nassaigael <hei.nassai@gmail.com>
2026-07-16 14:00:35 +0300

    feat(repository): add chart query methods
    Added two new query methods to the ChartRepository interface:
    * findByResultId: retrieves a chart by its result ID
    * findByChartType: retrieves a list of charts by their type
    These changes enhance the data retrieval capabilities of the repository.

--- src/main/java/com/henri_fraise/hff_data_studio/repository/ChartRepository.java
+++ src/main/java/com/henri_fraise/hff_data_studio/repository/ChartRepository.java
@@ -1,14 +1,19 @@
 package com.henri_fraise.hff_data_studio.repository;
 
 import com.henri_fraise.hff_data_studio.entity.Chart;
+import com.henri_fraise.hff_data_studio.enums.ChartType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ChartRepository  extends JpaRepository<Chart, UUID> {
-
 	
+	Optional<Chart> findByResultId(UUID resultId);
 
+	List<Chart> findByChartType(ChartType chartType);
+	
 }
