0dc4b7d5b9bbf538a6aba11818379208fe1b3e1b
nassaigael <hei.nassai@gmail.com>
2026-07-16 14:58:15 +0300

    feat(repository)!: update CustomUserRepository Update the CustomUserRepository interface to use UUID for user ID - - - Changes include updating the updateUserActivity method to use UUID instead of String for the user ID, and adding an import statement for java.util.UUID.

--- src/main/java/com/henri_fraise/hff_data_studio/repository/custom/CustomUserRepository.java
+++ src/main/java/com/henri_fraise/hff_data_studio/repository/custom/CustomUserRepository.java
@@ -3,10 +3,11 @@ package com.henri_fraise.hff_data_studio.repository.custom;
 import com.henri_fraise.hff_data_studio.dto.response.UserStatisticsResponse;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 public interface CustomUserRepository {
 	UserStatisticsResponse getUserStatistics();
 	long countActiveUsersByDateRange(LocalDateTime startDate, LocalDateTime endDate);
 	long countNewUsersByDateRange(LocalDateTime startDate, LocalDateTime endDate);
-	void updateUserActivity(String userId, String activity);
+	void updateUserActivity(UUID userId, String activity);
 }
