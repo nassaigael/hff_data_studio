d7f83ba08061be960d6efa3947fc97f59191640d
nassaigael <hei.nassai@gmail.com>
2026-07-16 14:53:13 +0300

    feat: add statistics response classes
    -
    -
    -
    The following classes have been added to handle statistics responses:
    - DatasetStatisticsResponse.java
    - ProjectStatisticsResponse.java
    - UserStatisticsResponse.java
    
    These classes utilize Lombok annotations for simplified data handling.

---
+++ src/main/java/com/henri_fraise/hff_data_studio/dto/response/ProjectStatisticsResponse.java
@@ -0,0 +1,19 @@
+package com.henri_fraise.hff_data_studio.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProjectStatisticsResponse {
+	private Long totalProjects;
+	private Long inProgress;
+	private Long completed;
+	private Long archived;
+	private Long totalCreators;
+	private Long newProjectsLast30Days;
+}
