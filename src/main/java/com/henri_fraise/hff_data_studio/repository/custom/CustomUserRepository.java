846d5a9f42d50c067435902875743af7c53565cc
nassaigael <hei.nassai@gmail.com>
2026-07-20 13:59:13 +0300

    refactor(repository): update countByMonth method
    Remove year parameter to simplify method signature and
    improve flexibility. This allows for more generic usage
    of the method. Resolves issue #123 from preprod branch.

--- src/main/java/com/henri_fraise/hff_data_studio/repository/custom/CustomUserRepository.java
+++ src/main/java/com/henri_fraise/hff_data_studio/repository/custom/CustomUserRepository.java
@@ -43,7 +43,7 @@ public interface CustomUserRepository {
 
 	List<Object[]> countGroupByCategoryWithDetails();
 
-	List<Object[]> countByMonth(int year);
+	List<Object[]> countByMonth();
 
 	List<Object[]> countByYear();
 
