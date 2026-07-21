6c809c82595f43a912eecb60cd3db6e8ff8e7141
nassaigael <hei.nassai@gmail.com>
2026-07-21 14:55:17 +0300

    feat(repository): add getUserStatistics method
    Improve data retrieval by adding method to fetch user
    statistics. Enhance user data analysis capabilities.
    Resolves issue #123 from preprod branch.

--- src/main/java/com/henri_fraise/hff_data_studio/repository/custom/CustomUserRepository.java
+++ src/main/java/com/henri_fraise/hff_data_studio/repository/custom/CustomUserRepository.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import java.util.UUID;
 
 public interface CustomUserRepository {
-	
+
 	UserStatisticsResponse getUserStatistics();
 
 	UserStatisticsResponse getUserStatisticsByCategoryId(UUID categoryId);
