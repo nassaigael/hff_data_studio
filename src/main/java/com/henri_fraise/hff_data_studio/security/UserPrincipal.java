f8165abbbd62fc3140978e00748afe79241bc806
nassaigael <hei.nassai@gmail.com>
2026-07-27 21:11:00 +0300

    feat(security): implement UserPrincipal class
    Add UserPrincipal to handle user authentication and
    authorization logic. Implement UserDetails interface
    to provide user details and authorities. This improves
    security and authentication in the application.

--- src/main/java/com/henri_fraise/hff_data_studio/security/UserPrincipal.java
+++ src/main/java/com/henri_fraise/hff_data_studio/security/UserPrincipal.java
@@ -1,4 +1,95 @@
 package com.henri_fraise.hff_data_studio.security;
 
-public class UserPrincipal {
+import com.henri_fraise.hff_data_studio.entity.Permission;
+import com.henri_fraise.hff_data_studio.entity.User;
+import com.henri_fraise.hff_data_studio.enums.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserPrincipal implements UserDetails {
+
+	private UUID userId;
+	private String email;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private Boolean isActive;
+	private UserRole role;
+	private List<String> permissions;
+
+	public static UserPrincipal create(User user) {
+		List<String> permissions = user.getCategory() != null && user.getCategory().getPermissions() != null
+				? user.getCategory().getPermissions().stream()
+				.map(Permission::getCode)
+				.collect(Collectors.toList())
+				: List.of();
+
+		return UserPrincipal.builder()
+				.userId(user.getId())
+				.email(user.getEmail())
+				.password(user.getPasswordHash())
+				.firstName(user.getFirstName())
+				.lastName(user.getLastName())
+				.isActive(user.getIsActive())
+				.role(user.getRole())
+				.permissions(permissions)
+				.build();
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<SimpleGrantedAuthority> authorities = permissions.stream()
+				.map(SimpleGrantedAuthority::new)
+				.collect(Collectors.toList());
+
+		if (role != null) {
+			authorities.add(new SimpleGrantedAuthority(role.name()));
+		}
+
+		return authorities;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return isActive != null && isActive;
+	}
 }
